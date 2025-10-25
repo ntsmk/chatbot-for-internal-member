320597d2fb5788e583306bc70bbd12bdc4b94309
Nat <1991.ntm@gmail.com>
2025-10-25 13:31:46 -0700

    Adding work in progress

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,19 @@
-# Storage Bucket
+# Creating Storage Bucket in GCP
 resource "google_storage_bucket" "chatbot_bucket" {
     name = "${var.project_id}-chatbot-bucket"
     location = "us-central1"
     force_destroy = true
 }
 
-# Artifact Registry for Docker images
-# todo add here
+# Creating Artifact Registry for Docker images in GCP
 resource "google_artifact_registry_repository" "chatbot_repo"{
     provider = google
+    location = "us-central1"
+    repository_id = "chatbot"
+    description = "Docker repository for chatbot app"
+    format = "DOCKER"
 
 }
 
-# Cloud Run Service with Docker image URL
+# Creating Cloud Run Service with Docker image URL in GCP
 # todo add here
