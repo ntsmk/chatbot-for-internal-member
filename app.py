10cce46885768ab851c614376daf88bdf7edc97f
Nat <1991.ntm@gmail.com>
2025-12-07 18:56:19 -0700

    Adding work in progress

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from app.chatbot import answer_question_supabase
+import os
 
 app = Flask(__name__)
 
@@ -23,4 +24,5 @@ def ask():
     return jsonify({"reply": response})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
