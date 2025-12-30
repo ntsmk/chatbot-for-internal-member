ca0336330968bb3725843bea867939cd0e0145d4
Nat <1991.ntm@gmail.com>
2025-12-29 17:10:34 -0700

    Adding work in progress

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,4 @@
 # Creating Storage Bucket in GCP
-# it complains there is already existing bucket. I deleted it
 resource "google_storage_bucket" "chatbot_bucket" {
     name = "${var.project_id}-chatbot-bucket"
     location = "us-central1"
@@ -7,8 +6,6 @@ resource "google_storage_bucket" "chatbot_bucket" {
 }
 
 # Creating Artifact Registry for Docker images in GCP
-# Got error, on GUI console, enabled it manually
-# Also it complains there is already existing registry. I deleted it
 resource "google_artifact_registry_repository" "chatbot_repo"{
     provider = google
     location = "us-central1"
